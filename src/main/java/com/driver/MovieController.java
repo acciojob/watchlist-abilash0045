a17fe607884d003ac0340fc64a5c019792451940
Abilash S L <abilash0045@gmail.com>
2023-01-26 19:53:49 +0530

    Implement Api's

---
+++ src/main/java/com/driver/MovieController.java
@@ -0,0 +1,59 @@
+package com.driver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+    @Autowired
+    MovieService movieService;
+    @PostMapping("/movies/add-movie")
+    public ResponseEntity addMovie(@RequestBody Movie movie){
+        String response = movieService.addMovie(movie);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+    @PostMapping("/movies/add-director")
+    public ResponseEntity addDirector(@RequestBody Director director){
+        String response = movieService.addDirector(director);
+        return new ResponseEntity<>(response,HttpStatus.CREATED);
+    }
+    @PutMapping("/movies/add-movie-director-pair")
+    public ResponseEntity addMovieDirectorPair(@RequestParam("movieName") String movieName,@RequestParam("directorName") String directorName){
+        String response = movieService.addMovieDirectorPair(movieName,directorName);
+        return new ResponseEntity<>(response,HttpStatus.ACCEPTED);
+    }
+    @GetMapping("/movies/get-movie-by-name/{name}")
+    public ResponseEntity getMovieByName(@PathVariable("name") String movieName){
+        Movie response = movieService.getMovieByName(movieName);
+        return new ResponseEntity<>(response,HttpStatus.FOUND);
+    }
+    @GetMapping("/movies/get-director-by-name/{name}")
+    public ResponseEntity getDirectorByName(@PathVariable("name") String directorName){
+        Director response = movieService.getDirectorByName(directorName);
+        return new ResponseEntity<>(response,HttpStatus.FOUND);
+    }
+    @GetMapping("/movies/get-movies-by-director-name/{director}")
+    public ResponseEntity getMoviesByDirectorName(@PathVariable("director") String directorName){
+        List<String> response = movieService.getMoviesByDirectorName(directorName);
+        return new ResponseEntity<>(response,HttpStatus.FOUND);
+    }
+    @GetMapping("/movies/get-all-movies")
+    public ResponseEntity findAllMovies(){
+        List<Movie> response = movieService.findAllMovies();
+        return new ResponseEntity<>(response,HttpStatus.FOUND);
+    }
+    @DeleteMapping("/movies/delete-director-by-name")
+    public ResponseEntity deleteDirectorByName(@RequestParam("name") String directorName){
+        String response = movieService.deleteDirectorByName(directorName);
+        return new ResponseEntity<>(response,HttpStatus.ACCEPTED);
+    }
+    @DeleteMapping("/movies/delete-all-directors")
+    public ResponseEntity deleteAllDirectors(){
+        String response = movieService.deleteAllDirectors();
+        return new ResponseEntity<>(response,HttpStatus.ACCEPTED);
+    }
+}
---
+++ src/main/java/com/driver/MovieRepository.java
@@ -0,0 +1,85 @@
+package com.driver;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class Pair{
+    Movie movie;
+    Director director;
+    Pair(Movie movie,Director director){
+        this.movie = movie;
+        this.director = director;
+    }
+}
+@Repository
+public class MovieRepository {
+    HashMap<String,Movie> movieHashMap = new HashMap<>();
+    HashMap<String,Director> directorHashMap = new HashMap<>();
+    HashMap<String,List<String>> pairHashMap = new HashMap<>();
+    public String  addMovie(Movie movie){
+        movieHashMap.put(movie.getName(),movie);
+        return "Movie added successfully";
+    }
+    public String addDirector(Director director){
+        directorHashMap.put(director.getName(),director);
+        return "Director added successfully";
+    }
+    public String addMovieDirectorPair(String movieName,String directorName){
+        if (pairHashMap.containsKey(directorName)){
+            List<String> movie = pairHashMap.get(directorName);
+            movie.add(movieName);
+            pairHashMap.put(directorName,movie);
+        }else{
+            List<String> list = new ArrayList<>();
+            list.add(movieName);
+            pairHashMap.put(directorName,list);
+        }
+        return "Pair added successfully";
+    }
+    public Movie getMovieByName(String movieName){
+        return movieHashMap.get(movieName);
+    }
+    public Director getDirectorByName(String directorName){
+        return directorHashMap.get(directorName);
+    }
+    public List<String> getMoviesByDirectorName(String directorName){
+        return pairHashMap.get(directorName);
+    }
+    public List<Movie> findAllMovies(){
+        List<Movie> movies = new ArrayList<>();
+        for (String movieName : movieHashMap.keySet()){
+            movies.add(movieHashMap.get(movieName));
+        }
+        return movies;
+    }
+    public String deleteDirectorByName(String directorName){
+        directorHashMap.remove(directorName);
+        if (pairHashMap.containsKey(directorName)){
+            List<String> movieNames = pairHashMap.get(directorName);
+            for (String movieName : movieNames){
+                if (movieHashMap.containsKey(movieName)){
+                    movieHashMap.remove(movieName);
+                }
+            }
+            pairHashMap.remove(directorName);
+        }
+        return "Removed successfully";
+    }
+    public String deleteAllDirectors(){
+        for (String directorName : pairHashMap.keySet()) {
+            List<String> movieNames = pairHashMap.get(directorName);
+            for (String movieName : movieNames) {
+                if (movieHashMap.containsKey(movieName)) {
+                    movieHashMap.remove(movieName);
+                }
+            }
+        }
+        directorHashMap.clear();
+        pairHashMap.clear();
+        return "All details deleted successfully";
+    }
+}
